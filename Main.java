b6f3e8d5d51d1726dc852da8f1e2529d6f2585a2
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2022-07-23 03:17:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+class Main {
+//                Methods: 
+//Its written inside a class.
+// Using of void method:
+  // static void lol(){
+  // System.out.println("bro i return nothing, which is void XD") ; 
+  // }
+/* int logic(int x, int y){
+    int z;
+    if(x>y){
+      z = x+y;
+    }
+    else {
+      z = (x + y) * 5;
+    }
+    return z;
+  } */
+  
+public static void main(String[] args) {
+  
+  //way 1 to call methods:
+ /*  int a = 3, b = 6;
+  int c = logic(a, b);
+  int a1=3, b1= 2;
+  int c1 = logic(a1, b1);
+  System.out.println(c);
+  System.out.println(c1); */
+
+  
+//way 2 to call methods:if we ain't using static.
+ /* int a = 3, b = 6;
+  Main obj = new Main();
+  int c = obj.logic(a, b);
+  int a1=3, b1= 2;
+  int c1 = obj.logic(a1, b1);
+  System.out.println(c);
+  System.out.println(c1); */
+// lol();
+    
+  }
+}
+
