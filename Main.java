af67b8aad5eef65a4a554e14e8130626c54ece71
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2022-07-23 04:00:26 +0000

    Method Overloading And VarArgs

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
   // static void lol(){
   // System.out.println("bro i return nothing, which is void XD") ; 
   // }
-/* int logic(int x, int y){
+/* static int logic(int x, int y){
     int z;
     if(x>y){
       z = x+y;
@@ -15,6 +15,30 @@ class Main {
     }
     return z;
   } */
+  
+  // static void change(int [] arr){
+  //   arr[0]= 87;
+  // }
+
+ //           Method Overloading:
+  // same name can be used to make methods but can still work as differnet.
+  // static void foo(){
+  //   System.out.printf("good morning bro!\n\n");
+  // }
+  // static void foo(int a){
+  //   System.out.printf("good morning\t" +a+ "\tbro!");
+  // }
+
+
+//               VarArgs:
+ // static int sum(int ...arr){
+ //   int result = 0;
+ //   for(int a: arr){
+ //     result += a;
+ //   }
+ //   return result;
+ // }
+
   
 public static void main(String[] args) {
   
@@ -36,7 +60,18 @@ public static void main(String[] args) {
   System.out.println(c);
   System.out.println(c1); */
 // lol();
-    
+
+  //changing the array
+  
+  // int [] arr= {12,44,22,66,88};
+  // change(arr);
+  // System.out.println(arr[0]);
+
+  // foo();
+  // int c = 1000;
+  // foo(c);
+
+  // System.out.printf("value of 2, 3, 5, 6:\t" + sum(2, 3, 5, 6));
   }
 }
 
